89df8946ea5e779c8b337e79c5f14012d87aa887
Abdo <abdulrahman.ali6666@gmail.com>
2024-11-04 18:55:19 +0300

    edits

--- js/app.js
+++ js/app.js
@@ -670,7 +670,7 @@ function toggleFAQ(question) {
       answer.style.opacity = '0'; // Fade out
       setTimeout(() => {
           answer.style.display = 'none'; // Hide answer after transition
-      }, 200); // Match this with CSS transition duration
+      },150); // Match this with CSS transition duration
       faqIcon.innerHTML = '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M12 6V18" stroke="#ffffff" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>';
 
       // Change the icon to indicate the FAQ is closed
