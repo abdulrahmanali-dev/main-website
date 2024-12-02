d4817110c7fa3a6bb7e638af919efe34642c27c0
Abdo <abdulrahman.ali6666@gmail.com>
2024-12-02 08:58:34 +0300

    asd

--- js/notificationEN.js
+++ js/notificationEN.js
@@ -1,1 +1,236 @@
-var template,notificationsList=[],baseClass="Notification",listElement=document.getElementsByClassName(baseClass+"s__List")[0],addBtn=document.getElementsByClassName(baseClass+"s__addBtn")[0],removeBtn=document.getElementsByClassName(baseClass+"s__removeBtn")[0],userName="You";function rnd(e){return Number.parseInt(Math.random()*e)}function Notification(e={}){var t=template.cloneNode(!0);function i(e,i,n,a){var o=t.getElementsByClassName(baseClass+e)[0];return("number"==typeof a&&(o=o.children[a]),"value"===i)?o.innerText=n:o.setAttribute(i,n)}return i("__Icon","src",e.icon,0),i("__Name","value",e.name),i("__Time","value",e.time),i("__Title","value",e.title),i("__Content","value",e.content),t}function addNotification(e,t){listElement.insertAdjacentElement("afterBegin",e),setTimeout(()=>{e.classList.remove(baseClass+"--Close"),setTimeout(()=>{e.classList.remove(baseClass+"--Optimize"),"function"==typeof t&&t();let i=document.querySelectorAll("."+baseClass);i.length>6&&listElement.removeChild(listElement.lastChild)},875)},25)}function removeNotification(e,t){e.classList.add(baseClass+"--Optimize"),e.classList.add(baseClass+"--Close"),setTimeout(()=>{e.remove(),"function"==typeof t&&t()},875)}function removeLastItem(){let e,t;t=document.getElementsByClassName(baseClass);for(let i=0;i<t.length;i++)if(!t[i].classList.contains(baseClass+"--Close")){e=t[i];break}removeNotification(e,()=>{listElement.children.length||(listElement.innerHTML="")})}function getCurrentTime(){var e=new Date;return e.getHours()+":"+(10>e.getMinutes()?"0":"")+e.getMinutes()}function generateRandomOrder(){var e=rnd(3)+1;return"You have a new order for "+e+" item"+(e>1?"s":"")+" totaling $"+(380.99*Math.random()+20).toFixed(2)}function addRandomNotification(){var e=notificationsList[rnd(notificationsList.length)];e.time=getCurrentTime(),e.content=generateRandomOrder(),addNotification(Notification(e))}function resizeChildDiv(){var e=$(".parent").width();$(".child").css("height",e*(95/440)+"px")}notificationsList.push({name:"Shopify",icon:"./images/shopify_logo_apple.png",time:"Now",title:userName,content:"You are a new order for 1 item totaling $365,99"},{name:"Shopify",icon:"./images/shopify_logo_apple.png",time:"Now",title:userName,content:"You are a new order for 1 item totaling $365,99"},{name:"Shopify",icon:"./images/shopify_logo_apple.png",time:"Now",title:userName,content:"You are a new order for 1 item totaling $365,99"},{name:"Shopify",icon:"./images/shopify_logo_apple.png",time:"Now",title:userName,content:"You are a new order for 1 item totaling $365,99"}),template=(()=>{let e=document.getElementsByClassName("Notification")[0],t=e.cloneNode(!0);return t.classList.add(baseClass+"--Close"),t.classList.add(baseClass+"--Optimize"),e.remove(),listElement.innerHTML="",t})(),addBtn.addEventListener("click",()=>{let e=Notification(notificationsList[rnd(notificationsList.length)]),t=document.querySelectorAll(".Notification");t.length>=10&&t[t.length-1].remove(),addNotification(e)}),removeBtn.addEventListener("click",removeLastItem),setInterval(()=>{},875/6),setTimeout(function(){var e=document.querySelector(".Notifications__List");e&&e.classList.add("visible")},5e3),addBtn.addEventListener("click",()=>{let e=Notification(notificationsList[rnd(notificationsList.length)]),t=document.querySelectorAll(".Notification");t.length>=10&&t[0].remove(),addNotification(e)});
+var template,
+  notificationsList = [],
+  baseClass = "Notification",
+  listElement = document.getElementsByClassName(baseClass + "s__List")[0],
+  addBtn = document.getElementsByClassName(baseClass + "s__addBtn")[0],
+  removeBtn = document.getElementsByClassName(baseClass + "s__removeBtn")[0],
+  userName = "You";
+
+notificationsList.push(
+  {
+    name: "Shopify",
+    icon: "./images/shopify_logo_apple.png",
+    time: "Now",
+    title: userName,
+    content: "You are a new order for 1 item totaling $365,99",
+  },
+  {
+    name: "Shopify",
+    icon: "./images/shopify_logo_apple.png",
+    time: "Now",
+    title: userName,
+    content: "You are a new order for 1 item totaling $365,99",
+  },
+  {
+    name: "Shopify",
+    icon: "./images/shopify_logo_apple.png",
+    time: "Now",
+    title: userName,
+    content: "You are a new order for 1 item totaling $365,99",
+  },
+  {
+    name: "Shopify",
+    icon: "./images/shopify_logo_apple.png",
+    time: "Now",
+    title: userName,
+    content: "You are a new order for 1 item totaling $365,99",
+  }
+);
+
+template = (() => {
+  let elm = document.getElementsByClassName("Notification")[0],
+    tmp = elm.cloneNode(true);
+
+  tmp.classList.add(baseClass + "--Close");
+  tmp.classList.add(baseClass + "--Optimize");
+
+  elm.remove();
+
+  listElement.innerHTML = "";
+
+  return tmp;
+})();
+
+function rnd(size) {
+  return Number.parseInt(Math.random() * size);
+}
+
+function Notification(config = {}) {
+  var element = template.cloneNode(true);
+
+  function handler(klass, attr, value, child) {
+    var tmp = element.getElementsByClassName(baseClass + klass)[0];
+
+    if (typeof child === "number") {
+      tmp = tmp.children[child];
+    }
+
+    if (attr === "value") {
+      return (tmp.innerText = value);
+    }
+
+    return tmp.setAttribute(attr, value);
+  }
+
+  handler("__Icon", "src", config.icon, 0);
+  handler("__Name", "value", config.name);
+  handler("__Time", "value", config.time);
+  handler("__Title", "value", config.title);
+  handler("__Content", "value", config.content);
+
+  return element;
+}
+
+function addNotification(notification, callback) {
+  // Ajoute la nouvelle notification au début de la liste
+  listElement.insertAdjacentElement("afterBegin", notification);
+
+  // Attend un court moment pour la transition
+  setTimeout(() => {
+    notification.classList.remove(baseClass + "--Close");
+    setTimeout(() => {
+      notification.classList.remove(baseClass + "--Optimize");
+
+      // Après les transitions, effectuez le callback si nécessaire
+      if (typeof callback === "function") callback();
+
+      // Vérifie maintenant le nombre de notifications après l'ajout
+      // Obtient toutes les notifications actuelles
+      const notifications = document.querySelectorAll("." + baseClass);
+      // Si le nombre dépasse 6, supprime la plus ancienne
+      if (notifications.length > 6) {
+        // Supprime la plus ancienne notification, qui est maintenant le dernier enfant dans la liste
+        listElement.removeChild(listElement.lastChild);
+      }
+    }, 875);
+  }, 25);
+}
+
+function removeNotification(notification, callback) {
+  notification.classList.add(baseClass + "--Optimize");
+  notification.classList.add(baseClass + "--Close");
+
+  setTimeout(() => {
+    notification.remove();
+
+    if (typeof callback === "function") callback();
+  }, 875);
+}
+
+function removeLastItem() {
+  let notification, tmp;
+
+  tmp = document.getElementsByClassName(baseClass);
+
+  for (let i = 0; i < tmp.length; i++) {
+    if (!tmp[i].classList.contains(baseClass + "--Close")) {
+      notification = tmp[i];
+      break;
+    }
+  }
+
+  removeNotification(notification, () => {
+    if (!listElement.children.length) {
+      listElement.innerHTML = "";
+    }
+  });
+}
+
+addBtn.addEventListener("click", () => {
+  // Créer une nouvelle notification
+  const newNotification = Notification(notificationsList[rnd(notificationsList.length)]);
+
+  // Vérifie le nombre de notifications actuelles
+  const notifications = document.querySelectorAll(".Notification");
+  if (notifications.length >= 10) {
+    // Si on a déjà 10 notifications, supprime la plus ancienne (la première enfant de la liste)
+    notifications[notifications.length - 1].remove();
+  }
+
+  // Ajoute la nouvelle notification au début de la liste
+  addNotification(newNotification);
+});
+
+removeBtn.addEventListener("click", removeLastItem);
+
+{
+  let c = 1,
+    end = 20,
+    operation = "add";
+
+  setInterval(() => {
+    return;
+
+    if (operation === "add") {
+      addNotification(Notification(notificationsList[rnd(notificationsList.length)]));
+    } else if (operation === "remove") {
+      removeLastItem();
+    }
+
+    if (c === 0) {
+      operation = "add";
+    } else if (c === end) {
+      operation = "remove";
+    }
+
+    if (operation === "add") {
+      c++;
+    } else if (operation === "remove") {
+      c--;
+    }
+  }, 875 / 6);
+}
+
+function getCurrentTime() {
+  var now = new Date();
+  return now.getHours() + ":" + (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
+}
+
+function generateRandomOrder() {
+  var items = rnd(3) + 1; // Génère un nombre aléatoire d'articles entre 1 et 3
+  var amount = (Math.random() * (400.99 - 20.0) + 20.0).toFixed(2); // Génère un montant aléatoire entre $20,00 et $400,99
+  return (
+    "You have a new order for " + items + " item" + (items > 1 ? "s" : "") + " totaling $" + amount
+  );
+}
+
+// Modifier la fonction addRandomNotification pour utiliser les nouvelles fonctions
+function addRandomNotification() {
+  var randomIndex = rnd(notificationsList.length);
+  var notification = notificationsList[randomIndex];
+
+  // Met à jour le temps et le contenu de la notification
+  notification.time = getCurrentTime(); // Utilise l'heure actuelle
+  notification.content = generateRandomOrder(); // Génère un contenu de commande aléatoire
+
+  var newNotification = Notification(notification);
+  addNotification(newNotification);
+}
+
+function resizeChildDiv() {
+  var parentWidth = $(".parent").width(); // Largeur du parent
+  var childHeight = parentWidth * (95 / 440); // Calcul de la hauteur basée sur le ratio
+  $(".child").css("height", childHeight + "px"); // Appliquer la hauteur calculée à l'enfant
+}
+
+setTimeout(function () {
+  var element = document.querySelector(".Notifications__List");
+  if (element) {
+    element.classList.add("visible");
+  }
+}, 5000); // 1000 millisecondes = 1 seconde
+
+addBtn.addEventListener("click", () => {
+  // Créer une nouvelle notification
+  const newNotification = Notification(notificationsList[rnd(notificationsList.length)]);
+
+  // Vérifie le nombre de notifications actuelles
+  const notifications = document.querySelectorAll(".Notification");
+  if (notifications.length >= 10) {
+    // Si on a déjà 10 notifications, supprime la plus ancienne (la première enfant de la liste)
+    notifications[0].remove();
+  }
+
+  // Ajoute la nouvelle notification au début de la liste
+  addNotification(newNotification);
+});
